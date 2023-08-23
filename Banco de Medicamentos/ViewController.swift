09d558f707981b4a4fb54909f38413122ab95070
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2023-08-23 00:53:27 -0500

    Initial Commit

---
+++ Banco de Medicamentos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banco de Medicamentos
+//
+//  Created by user246310 on 8/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
