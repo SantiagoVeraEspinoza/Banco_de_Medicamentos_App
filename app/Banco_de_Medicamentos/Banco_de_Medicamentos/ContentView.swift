97a5e856adf0a239acf83f7d9e169fc2a16c31df
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2023-08-24 23:28:08 -0500

    Now we are using SwiftUI

---
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Banco_de_Medicamentos
+//
+//  Created by user246310 on 8/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
