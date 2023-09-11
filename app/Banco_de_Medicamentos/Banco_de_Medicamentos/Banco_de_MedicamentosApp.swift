e4f7147ef75a4934c61a2d29a056562ae9d7084d
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2023-09-11 18:21:08 -0500

    Cambiar nombre de pantalla inicial

--- app/Banco_de_Medicamentos/Banco_de_Medicamentos/Banco_de_MedicamentosApp.swift
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/Banco_de_MedicamentosApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Banco_de_MedicamentosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreen()
         }
     }
 }
