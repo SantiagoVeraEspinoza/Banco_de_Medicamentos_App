94def65a4d01aedacd7419d388b99df9e11afdd7
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2023-09-11 18:11:35 -0500

    Anadir vista de acuerdos

---
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/Acuerdos.swift
@@ -0,0 +1,27 @@
+//
+//  Acuerdos.swift
+//  Banco_de_Medicamentos
+//
+//  Created by user245583 on 9/11/23.
+//
+
+import SwiftUI
+
+struct Acuerdos: View {
+    var body: some View {
+        VStack{
+            Spacer()
+            Image("logo")
+            Text("El Banco de Alimentos se enorgullece de presentarte un nuevo servicio para el bienestar de ti y tu familia. Con la confianza del Banco de Alimentos, ahora para medicamentos.").multilineTextAlignment(.center).foregroundColor(.gray)
+            Spacer()
+            Button("Iniciar"){}.buttonStyle(.borderedProminent)
+            Button("Aviso de privacidad") {}
+        }.padding()
+    }
+}
+
+struct Acuerdos_Previews: PreviewProvider {
+    static var previews: some View {
+        Acuerdos()
+    }
+}
--- app/Banco_de_Medicamentos/Banco_de_Medicamentos/SplashScreen.swift
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/SplashScreen.swift
@@ -7,20 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SplashScreen: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("logo")
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        SplashScreen()
     }
 }
