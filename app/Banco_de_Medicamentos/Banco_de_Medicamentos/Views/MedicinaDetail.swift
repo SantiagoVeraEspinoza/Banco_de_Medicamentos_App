9a8b63e7ba50d29e5665f71a4e0e42ec207250e1
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2023-09-22 01:31:45 -0500

    crear pagina de detalles y lista de medicamentos

---
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/Views/MedicinaDetail.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct MedicinaDetail: View {
+    var medicina:Medicina
+    var body: some View {
+        ScrollView{
+            
+            CircleImage(ImageUrl: medicina.imageUrl).ignoresSafeArea(edges: .top).frame(height: 300)
+            
+            VStack(alignment: .leading){
+                Text(medicina.name).font(.title)
+                Divider()
+                Text("Acerca de \(medicina.name)").font(.title2)
+                Text(medicina.descripcion)
+                Divider()
+                if(medicina.prescripcion){
+                    Text("\(Image(systemName: "exclamationmark.triangle")) Prescripcion necesaria").foregroundStyle(.red).bold()
+                }
+            }.padding()
+        }
+        .navigationTitle(medicina.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct MedicinaDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MedicinaDetail(medicina: medicinas[1])
+    }
+}
