f7899ea31e42670e40592347a08f65f256873eba
Santiago Vera <santiveraespinoza@gmail.com>
2023-10-03 23:25:46 -0600

    Now the app gets real time updates

--- app/Banco_de_Medicamentos/Banco_de_Medicamentos/FirestoreManager.swift
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/FirestoreManager.swift
@@ -9,14 +9,30 @@ import Foundation
 import Firebase
 import FirebaseFirestore
 
+struct Inventario: Identifiable {
+    var id: String
+    
+    var idMedicamento: String
+    var idCentro: String
+    var disponible: Bool
+    var peligro: Bool
+
+    init(id: String, idMedicamento: String, idCentro: String, disponible: Bool, peligro: Bool) {
+        self.id = id
+        self.idMedicamento = idMedicamento
+        self.idCentro = idCentro
+        self.disponible = disponible
+        self.peligro = peligro
+    }
+}
+
 class FirestoreManager: ObservableObject {
     
-    @Published var test: String = ""
+    private var db = Firestore.firestore()
+    @Published var data: Inventario?
     
-    func fetchDoc() {
-        let db = Firestore.firestore()
-
-        let docRef = db.collection("test").document("glIDVg6IkwdpgOfdpwN8")
+    func fetchDB() {
+        let docRef = db.collection("BancoDeMedicamentos").document("fI6Pke7q2ZhMboi5HkC1").collection("Inventario").document("G3oT7wIi0IpmE0hzkD7d")
 
         docRef.getDocument { (document, error) in
             guard error == nil else {
@@ -28,28 +44,72 @@ class FirestoreManager: ObservableObject {
                 let data = document.data()
                 if let data = data {
                     print("data", data)
-                    self.test = data["name"] as? String ?? ""
+                    if let disponible = data["disponible"] as? Bool {
+                        self.data?.disponible = disponible
+                    }
+                    //self.data = data["disponible"] as? String ?? ""
                 }
             }
         }
     }
     
-    func fetchAllDocs() {
-            let db = Firestore.firestore()
+    func fecthAllInventories() {
+        db.collection("BancoDeMedicamentos").document("fI6Pke7q2ZhMboi5HkC1").collection("Inventario").getDocuments() { (querySnapshot, error) in
+                        if let error = error {
+                                print("Error getting documents: \(error)")
+                        } else {
+                                for document in querySnapshot!.documents {
+                                        print("\(document.documentID): \(document.data())")
+                                }
+                        }
+        }
+    }
+    
+    func fetchCenterFromInventory(center_id: String){
 
-            db.collection("test").getDocuments() { (querySnapshot, error) in
-                            if let error = error {
-                                    print("Error getting documents: \(error)")
-                            } else {
-                                    for document in querySnapshot!.documents {
-                                            print("\(document.documentID): \(document.data())")
-                                    }
-                            }
+        let docRef = db.collection("BancoDeMedicamentos").document("fI6Pke7q2ZhMboi5HkC1").collection("Centro").document(center_id)
+
+        docRef.getDocument { (document, error) in
+            guard error == nil else {
+                print("error", error ?? "")
+                return
+            }
+
+            if let document = document, document.exists {
+                let data = document.data()
+                if let data = data {
+                    print("data", data)
+                    //self.data. = data["nombre"] as? String ?? ""
+                }
+            }
+        }
+    }
+    
+    func fetchData(){
+        db.collection("BancoDeMedicamentos").document("fI6Pke7q2ZhMboi5HkC1").collection("Inventario").document("G3oT7wIi0IpmE0hzkD7d")
+            .addSnapshotListener { documentSnapshot, error in
+                guard let document = documentSnapshot else {
+                    print("Error fetching document: \(error!)")
+                    return
+                }
+                guard let data = document.data(),
+                    let idMedicamento = data["idMedicamento"] as? String,
+                    let idCentro = data["idCentro"] as? String,
+                    let disponible = data["disponible"] as? Bool,
+                    let peligro = data["peligro"] as? Bool else {
+                    print("Document data was empty.")
+                    return
+                }
+                let id = document.documentID
+                let inventario = Inventario(id: id, idMedicamento: idMedicamento, idCentro: idCentro, disponible: disponible, peligro: peligro)
+                DispatchQueue.main.async {
+                    self.data = inventario
+                }
             }
     }
     
     init() {
-        fetchDoc()
-        fetchAllDocs()
+        fetchDB()
+        fecthAllInventories()
     }
 }
--- app/Banco_de_Medicamentos/Banco_de_Medicamentos/Views/Home.swift
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/Views/Home.swift
@@ -6,13 +6,22 @@ import SwiftUI
 struct SecondView: View {
     @EnvironmentObject var firestoreManager: FirestoreManager
     
+    //firestoreManager.fetchCenterFromInventory(center_id: "ImMQGZrYKCY9NbFUgIim")
+    
     var body: some View {
         VStack{
             Spacer()
             Image("logo")
             Text("El Banco de Alimentos se enorgullece de presentarte un nuevo servicio para el bienestar de ti y tu familia Con la confianza del Banco de Alimentos, ahora para medicamentos.")
-            Text("My test: \(firestoreManager.test)")
+            Text("My test: \(firestoreManager.data?.id ?? "")")
                     .padding()
+            
+            Text("Data from Firestore: \(firestoreManager.data?.idCentro ?? "")")
+                        .onAppear {
+                            // Call fetchData when the view appears
+                            firestoreManager.fetchData()
+                        }
+            
             Spacer()
             NavigationLink(destination: MainMenu()){
                 Text("Iniciar")
