e9609eb32a5cc6ff82b5ea73ebe2f25da475ff43
racs11 <58236987+racs11@users.noreply.github.com>
2023-10-05 22:02:03 -0500

    front

--- app/Banco_de_Medicamentos/Banco_de_Medicamentos/Views/MainMenu.swift
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/Views/MainMenu.swift
@@ -18,13 +18,16 @@ struct MainMenu: View {
                             .scaledToFill()
                             //.edgesIgnoringSafeArea(.all)
                             .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                            .blur(radius: 3)
+                            .grayscale(0.50)
                             
 
                         
                         Text("Medicinas").bold()
+                            //.foregroundColor(.white)
                             .shadow(color: .white, radius: 1)
                             .shadow(color: .white, radius: 1)
-                            .shadow(color: .white, radius: 1)
+                            //.shadow(color: .white, radius: 1)
                
                             .font(.system(.largeTitle, design: .rounded).weight(.heavy))
                             //.foregroundStyle(.black)
@@ -35,18 +38,21 @@ struct MainMenu: View {
                     }
                 }
             }
-            Spacer()
+            
             NavigationLink(destination: CentroList()){
                 GeometryReader{geo in
                     ZStack{
                         Image("centro_img").scaledToFill()
                         //.edgesIgnoringSafeArea(.all)
                         .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                        .blur(radius: 3)
+                        .grayscale(0.50)
                         
                         Text("Centros").bold()
+                            //.foregroundColor(.white)
                             .shadow(color: .white, radius: 1)
                             .shadow(color: .white, radius: 1)
-                            .shadow(color: .white, radius: 1)
+                            //.shadow(color: .white, radius: 1)
                            
                             .font(.system(.largeTitle, design: .rounded).weight(.heavy))
                             //.foregroundColor(.black)
