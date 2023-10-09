934cd0195b2b1b15571f8dd149c72a7d91cf48e7
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2023-10-09 10:28:29 -0700

    unplug firebase connection and reconfigure location json to strings

--- app/Banco_de_Medicamentos/Banco_de_Medicamentos/Views/CentroDetail.swift
+++ app/Banco_de_Medicamentos/Banco_de_Medicamentos/Views/CentroDetail.swift
@@ -3,15 +3,16 @@ import CoreLocation
 
 struct CentroDetail: View {
     var centro:Centro
-    
     var body: some View {
+        let floatLatitude = (centro.latitude as NSString).doubleValue
+        let floatLongitude = (centro.longitude as NSString).doubleValue
         var locationCoordinate: CLLocationCoordinate2D {
             CLLocationCoordinate2D(
-                latitude: centro.latitude, longitude: centro.longitude
+                latitude: floatLatitude, longitude: floatLongitude
             )
         }
         
-        let url = URL(string: "maps://?saddr=&daddr=\(centro.latitude),\(centro.longitude)")
+        let url = URL(string: "maps://?saddr=&daddr=\(floatLatitude),\(floatLongitude)")
         ScrollView {
             MapView(coordinate: locationCoordinate).ignoresSafeArea(edges: .top).frame(height: 300)
             CircleImage(ImageUrl: centro.imageUrl).ignoresSafeArea(edges: .top).offset(y: -130).padding(.bottom, -130)
